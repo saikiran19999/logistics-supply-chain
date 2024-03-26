9b00e28691781c5f5d1bc72c0a2f8f1fe5684cda
saikiran19999 <50092036+saikiran19999@users.noreply.github.com>
2024-03-26 10:31:19 -0400

    Update db_connect.php

--- ui-service/db_connect.php
+++ ui-service/db_connect.php
@@ -5,7 +5,7 @@ $username = "ranjit";
 $password = "ranjit";
 $db = "myDb";
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
