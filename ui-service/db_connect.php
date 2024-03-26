325139e42adde0865096fa4f1207acb0a5cc8cc8
saikiran19999 <50092036+saikiran19999@users.noreply.github.com>
2024-03-26 09:18:40 -0400

    Update db_connect.php

--- ui-service/db_connect.php
+++ ui-service/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('ranjit,'ranjit','ranjit','ranjit')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('ranjit','ranjit','ranjit','ranjit')or die("Could not connect to mysql".mysqli_error($con));
