58c5fb9c55670f7743f469de878e5cc38d0c1285
saikiran19999 <50092036+saikiran19999@users.noreply.github.com>
2024-03-26 10:00:46 -0400

    Update db_connect.php

--- ui-service/db_connect.php
+++ ui-service/db_connect.php
@@ -1,11 +1,11 @@
 <?php
 
-$hostname = 'ranjit'; // This should be the name of the MySQL service defined in your Docker Compose file
-$username = 'ranjit'; // MySQL username
-$password = 'ranjit'; // MySQL password
-$database = 'ranjit'; // MySQL database name
+$servername = "db";
+$username = "sai";
+$password = "sai";
+$db = "database";
 
-$conn = new mysqli($hostname, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
